063739ffd4cfc05e684ba15ef816ccb509bd3154
Sears Johnston <Sears Johnston>
2017-03-18 12:28:20 -0400

    Added material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<h1>Hello {{name}}</h1>` + 
+  '<md-spinner></md-spinner>',
 })
 export class AppComponent  { name = 'Angular'; }
